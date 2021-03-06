33e83aebe2c0ee24550c9d1922517e8813880895
KimBitrus26 <bitruschoji4real@gmail.com>
2021-03-06 21:04:34 +0100

    first

--- kimecommerce/settings.py
+++ kimecommerce/settings.py
@@ -76,7 +76,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #whitenoise.django.GzipManifestStaticFilesStorage'
 DBUS_CFLAGS= False
 DBUS_LIBS =False
 
