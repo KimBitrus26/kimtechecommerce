486326dcbf843a2db2ca0f79ccf1d4057f0cb8c8
KimBitrus26 <bitruschoji4real@gmail.com>
2021-03-08 08:32:41 +0100

    first

--- kimecommerce/settings.py
+++ kimecommerce/settings.py
@@ -6,7 +6,7 @@ from whitenoise import WhiteNoise
 
 #ENVIRONMENT = os.getenv('ENVIRONMENT', 'production')
 
-DEBUG = False #True
+DEBUG = True
 #DEBUG = config('DEBUG', default=False, cast=bool)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
